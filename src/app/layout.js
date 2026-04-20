181eb4d3c33949ec08d546568f8b7768365bc598
md-morsalin10 <mdmorsalinofficial10@gmail.com>
2026-04-21 00:55:14 +0600

    theme toggle complted

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import ThemeProviders from "@/provider/ThemeProviders";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,8 +23,14 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col bg-background text-foreground">
+        <ThemeProviders>
+          <Navbar />
+          {children}
+        </ThemeProviders>
+      </body>
     </html>
   );
 }
--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,11 +1,17 @@
+import TasksCard from "@/components/TasksCard";
 import { getTask } from "@/lib/tasks";
 
 
 const TasksData =async () => {
     const tasks =await getTask();
     return (
-        <div>
+        <div className="container mx-auto py-8">
             <p>Tasks:{tasks.length}</p>
+            <div className="grid grid-cols-3 gap-5">
+                {
+                    tasks.map((task,ind)=> <TasksCard task={task} key={ind}></TasksCard>)
+                }
+            </div>
         </div>
     );
 };
